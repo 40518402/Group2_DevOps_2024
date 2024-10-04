d7fc7b2f3f9f985925010cadd4d48009a1b751ce
40736622 <40736622@live.napier.ac.uk>
2024-10-03 18:47:13 -0600

    updated app and docker files to work with mysql database

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,6 @@
 FROM openjdk:latest
-COPY ./target/classes/com /tmp/com
+COPY ./target/seMethods-0.1.0.1-jar-with-dependencies.jar /tmp
 WORKDIR /tmp
-ENTRYPOINT ["java", "com.napier.devops.Apps"]
+# change last parameter to 10000 if running on github actions
+#if running locally and database aleady started delay can be zero
+ENTRYPOINT ["java", "-jar", "seMethods-0.1.0.1-jar-with-dependencies.jar", "world:3306", "10000"]
--- src/main/java/com/napier/devops/Apps.java
+++ src/main/java/com/napier/devops/Apps.java
@@ -1,9 +1,83 @@
 package com.napier.devops;
 
-public class Apps {
+import java.sql.*;
 
-        public static void main(String[] args)
+public class Apps
+{
+    /**
+     * Connection to MySQL database.
+     */
+    private Connection con = null;
+
+    /**
+     * Connect to the MySQL database.
+     */
+    public void connect()
+    {
+        try
         {
-            System.out.println("Hello and welcome! to Group 2");
+            // Load Database driver
+            Class.forName("com.mysql.cj.jdbc.Driver");
         }
+        catch (ClassNotFoundException e)
+        {
+            System.out.println("Could not load SQL driver");
+            System.exit(-1);
+        }
+
+        int retries = 10;
+        for (int i = 0; i < retries; ++i)
+        {
+            System.out.println("Connecting to database...");
+            try
+            {
+                // Wait a bit for db to start
+                Thread.sleep(30000);
+                // Connect to database
+                con = DriverManager.getConnection("jdbc:mysql://db:3306/world?allowPublicKeyRetrieval=true&useSSL=false", "root", "example");
+                System.out.println("Successfully connected");
+                break;
+            }
+            catch (SQLException sqle)
+            {
+                System.out.println("Failed to connect to database attempt " + Integer.toString(i));
+                System.out.println(sqle.getMessage());
+            }
+            catch (InterruptedException ie)
+            {
+                System.out.println("Thread interrupted? Should not happen.");
+            }
+        }
+    }
+
+    /**
+     * Disconnect from the MySQL database.
+     */
+    public void disconnect()
+    {
+        if (con != null)
+        {
+            try
+            {
+                // Close connection
+                con.close();
+            }
+            catch (Exception e)
+            {
+                System.out.println("Error closing connection to database");
+            }
+        }
+    }
+
+    public static void main(String[] args)
+    {
+        // Create new Application
+        Apps a = new Apps();
+
+        // Connect to database
+        a.connect();
+
+        // Disconnect from database
+        a.disconnect();
+    }
 }
