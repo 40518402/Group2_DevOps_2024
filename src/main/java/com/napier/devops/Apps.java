8717fef7ce03d2c5d068a5263c7a55c17afc4383
Devin Izair Vivas <40734852@live.napier.ac.uk>
2024-11-15 15:44:39 -0600

    Added Labels to the outputs

--- src/main/java/com/napier/devops/Apps.java
+++ src/main/java/com/napier/devops/Apps.java
@@ -16,36 +16,37 @@ public class Apps
         // Connect to database
         ctryReport.connect();
 
+        System.out.println("Getting the top N (10) populated countries in a continent (Asia) where N is provided by the user.");
         // Getting the top N populated countries in a continent where N is provided by the user.
         ArrayList<CountryReport> topCountries = ctryReport.getCountriesByContinent("Asia", 10);
         ctryReport.displayCountries(topCountries);
 
         System.out.println();
-
+        System.out.println("Getting all the countries in a continent (Asia) organised by largest population to smallest");
         // Getting all the countries in a continent organised by largest population to smallest.
         ArrayList<CountryReport> countries = ctryReport.getCountriesByContinent("Asia");
         ctryReport.displayCountries(countries);
 
         System.out.println();
-
+        System.out.println("Getting All Countries In a Region (Caribbean)");
         // Getting All Countries In a Region
         ArrayList<CountryReport> region = ctryReport.getCountriesByRegion("Caribbean");
         ctryReport.displayCountries(region);
 
         System.out.println();
-
+        System.out.println("Getting the top N (10) populated countries in a region (Caribbean) where N is provided by the user.");
         // Getting the top N populated countries in a region where N is provided by the user.
         ArrayList<CountryReport> topCountriesByRegion = ctryReport.getTopCountriesByRegion("Caribbean", 10);
         ctryReport.displayCountries(topCountriesByRegion);
 
         System.out.println();
-
+        System.out.println("Get the top N (10) populated countries in the world where N is provided by the user.");
         // Get the top N populated countries in the world where N is provided by the user.
         ArrayList<CountryReport> topCountriesInWorld = ctryReport.getCountriesInWorld(10);
         ctryReport.displayCountries(topCountriesInWorld);
 
         System.out.println();
-
+        System.out.println("Get all the countries in the world organised by largest population to smallest");
         // Get all the countries in the world organised by largest population to smallest.
         ArrayList<CountryReport> countriesInWorld = ctryReport.getCountriesByWorldPopulation();
         ctryReport.displayCountries(countriesInWorld);
